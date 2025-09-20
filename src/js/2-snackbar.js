82520cf9ef1764e4fccc60b4004b9a4d74994fc2
Alina <alinarybalchenko@hotmail.com>
2025-09-20 21:12:57 +0200

    snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,35 @@
-console.log('Snackbar');
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  createPromise(delay, state)
+    .then(ms => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topCenter',
+      });
+    })
+    .catch(ms => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topCenter',
+      });
+    });
+
+  form.reset();
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      state === 'fulfilled' ? resolve(delay) : reject(delay);
+    }, delay);
+  });
+}
